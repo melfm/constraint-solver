5996d601b419a42b8b69ed852275e0edde4292fe
Melissa <melissafm24@gmail.com>
2014-04-12 23:23:47 +0100

    And thats it... simplezz

--- src/core/Backup.java
+++ src/core/Backup.java
@@ -16,7 +16,36 @@ public class Backup {
 	private final Map<String, Set<Integer>> saved = new LinkedHashMap<String, Set<Integer>>();
 	private Integer cursor;
 	
+	// 
+	// Constructor
+	//
+	public Backup(Solver solver) {
+		save(solver);
+	}
 	
-	
+	//
+	// Save variable value sets in the solver
+	//
+	public void save(Solver solver) {
+		// Make copies of variable values sets along with their names
+		this.saved.clear();
+		for (IntVariable var : solver.allVariables()) {
+			Set<Integer> copy = new LinkedHashSet<Integer>(var.valueSet());
+			this.saved.put(var.name(), copy);
+		}
+		this.cursor = solver.cursor();
+	}
+	//
+	// Restore variable value sets back to the solver
+	//
+	public void restore(Solver solver){
+		// Copy saved value sets back to the variable in the solver
+		for (IntVariable var : solver.allVariables()) {
+			assert (saved.containsKey(var.name()));
+			var.valueSet().clear();
+			var.valueSet().addAll(this.saved.get(var.name()));
+		}
+		solver.setCursor(this.cursor);
+	}
 	
 }
--- src/core/Solver.java
+++ src/core/Solver.java
@@ -65,14 +65,27 @@ public class Solver {
 	// Create variables in this solver context
 	//
 	public IntVariable createIntVariable(String name, int low, int high) throws Exception {
-		return null;
+		// Make sure variable is not already defined!
+		if (this.varibaleNames.contains(name)) {
+			throw new Exception("Variable already exists.");
+		}
+		IntVariable variable = new IntVariable(name, low, high);
+		this.varibaleNames.add(variable.name());
+		this.allVariables.add(variable);
+		return variable;
 	}
 	
 	//
 	// Add a constraint
 	//
 	public void addConstraint(Constraint constraint) throws Exception {
-		
+		// Make sure variables in the constraint are defined
+		for (IntVariable v : constraint.variables()) {
+			if (!this.varibaleNames.contains(v.name())) {
+				throw new Exception("Variable not defined in the solver.");
+			}
+		}
+		this.allConstraints.add(constraint);
 	}
 	
 	//
@@ -80,7 +93,15 @@ public class Solver {
 	// This calls the recursive guy
 	//
 	public void search() {
-		
+		// Init variables and start search
+		for (IntVariable var : this.allVariables) {
+			var.initialise();
+			var.dump();
+		}
+		// Start exploring search space
+		searchRecursive();
+		// If recursive search completes in full, then solution not found
+		System.out.println("Solution not found :(");
 	}
 	
 	//
@@ -88,24 +109,57 @@ public class Solver {
 	// This guy is recursive
 	//
 	private void searchRecursive() {
-		
+		// Get the next variable possibly with some heuristic
+		IntVariable currentVar = getNextVariable();
+		if (currentVar != null) {
+			// Try assigning each value from the current value set
+			// Make a copy of the value set as it gets overwitten
+			Set<Integer> valueSet = new LinkedHashSet<Integer>(currentVar.valueSet());
+			for (int value : valueSet) {
+				// Backup variables
+				Backup backup = new Backup(this);
+				//Assign value to the variable
+				if (currentVar.assignAndPropagate(value)) {
+					// If successful, check if all variables have been reduced
+					// to a single value. If that is the case and every variable
+					// has been processed, then we have found a solution!
+					if (!isNextVariable() && checkAllVariablesAreSingle()) {
+						printSolution();
+						System.exit(0);
+					}
+					// Otherwise recurse to contingue search
+					searchRecursive();
+				}
+				// Resotre variables
+				backup.restore(this);
+			}
+			
+		}
 	}
 	
 	//
 	// Return next variable
 	//
 	private IntVariable getNextVariable() {
+		if (isNextVariable()) {
+			return this.allVariables.get(this.cursor++);
+		}
 		return null;
 	}
 	
 	private boolean isNextVariable() {
-		return false;
+		return this.cursor < this.allVariables.size();
 	}
 	
 	//
 	// Check whether all variables are single
 	//
 	private boolean checkAllVariablesAreSingle() {
+		for (IntVariable v : this.allVariables) {
+			if (v.valueSet().size() != 1) {
+				return false;
+			}
+		}
 		return true;
 	}
 	
