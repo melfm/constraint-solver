81a48f7faa627df9221d3dc4198167e0b17a86b0
Melissa <melissafm24@gmail.com>
2014-04-12 22:55:51 +0100

    Solver skeleton

--- src/core/Solver.java
+++ src/core/Solver.java
@@ -1,6 +1,7 @@
 package core;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
@@ -17,19 +18,106 @@ import constraints.Constraint;
  */
 public class Solver {
 	// Fields
-	private final Map<String, IntVariable> intvariables = new LinkedHashMap<String, IntVariable>();
-	private final List<Constraint> allconstraints = new ArrayList<Constraint>();
-	private Iterator<IntVariable> variableIterator;
+	private final List<Constraint> allConstraints = new ArrayList<Constraint>();
+	private final List<IntVariable> allVariables = new ArrayList<IntVariable>();
+	private final Set<String> varibaleNames = new HashSet<String>();
+	
+	private int cursor;
 
 	// Constructor
 	public Solver() {
 
 	}
 
-	public Map<String, IntVariable> allVariables() {
-		return this.intvariables;
+	// Getters
+	public List<IntVariable> allVariables() {
+		return this.allVariables;
+	}
+	
+	public List<Constraint> allconstraints() {
+		return this.allConstraints;
+	}
+	
+	public Set<String> variableNames() {
+		return this.varibaleNames;
 	}
 
+	public int cursor() {
+		return this.cursor;
+	}
 	
+	// Setters
+	public void setCursor(int cursor) {
+		this.cursor = cursor;
+	}
 
+	//
+	// Reset solver state
+	//
+	public void reset() {
+		this.allConstraints.clear();
+		this.allVariables.clear();
+		this.varibaleNames.clear();
+		this.cursor = 0;
+	}
+	
+	//
+	// Create variables in this solver context
+	//
+	public IntVariable createIntVariable(String name, int low, int high) throws Exception {
+		return null;
+	}
+	
+	//
+	// Add a constraint
+	//
+	public void addConstraint(Constraint constraint) throws Exception {
+		
+	}
+	
+	//
+	// Search for a solution
+	// This calls the recursive guy
+	//
+	public void search() {
+		
+	}
+	
+	//
+	// Explore search space using Forward Checking
+	// This guy is recursive
+	//
+	private void searchRecursive() {
+		
+	}
+	
+	//
+	// Return next variable
+	//
+	private IntVariable getNextVariable() {
+		return null;
+	}
+	
+	private boolean isNextVariable() {
+		return false;
+	}
+	
+	//
+	// Check whether all variables are single
+	//
+	private boolean checkAllVariablesAreSingle() {
+		return true;
+	}
+	
+	//
+	// Print solution
+	//
+	private void printSolution() {
+		System.out.println("Found a solution!");
+		System.out.println("----------------");
+		for (IntVariable v : this.allVariables) {
+			System.out.println(v.name() + ":" + v.valueSet());
+		}
+		System.out.println("----------------");
+	}
 }
